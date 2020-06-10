9776e12b5ea449e78519049eca613ca2d1b06b4c
BakaFT <1355564740@qq.com>
2020-06-10 15:04:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# 简介
+
+一个能够实现在学堂在线(next.xuetang.com) 自动完成作业的小脚本
+
+如果喜欢，请给我一个星星 :)
+
+# 测试环境
+
+- Python 3.7.4
+- Windows 10 1909
+- Requests 2.22.0
+
+```bash
+pip(3) install requests
+```
+
+- Uncurl
+
+```bash
+pip(3) install uncurl
+```
+
+# 功能实现
+
+- 通过`Curl`进行登录
+- 列出当前学习的课程列表并使用序号选择
+- 自动跳过已经做过的题
+- 自动跳过简答题
+- 答题间隔`3s`以防被ban
+
+# 使用注意
+
+## 提取Curl
+
+因为学堂在线的登录采用`TencentCaptcha`，目前没有什么思路去实现模拟登录，所以需要手动提取一个Curl来为程序提供身份验证信息。 
+
+提取的`curl`中的`cookie`需要包含`x-csrftoken`，否则会出现跨域请求错误。
+
+### Chrome(Chromium) 下的提取
+
+打开课程页面，比如
+
+```
+https://next.xuetangx.com/learn/NUDT12041000081/NUDT12041000081/1510792/
+```
+
+按下`F12`以打开`Chrome developer tools`,  并且切换到`Network`选项卡
+
+如下操作打开`Filter` 并选中`XHR`类型
+
+![README](README.png)
+
+然后刷新页面，等待加载完毕。
+
+寻找任意一条，如图所示，在`Headers`属性卡里包含`x-csrftoken`的请求
+
+![README2](README2.png)
+
+右键这条请求，依次选择`Copy` - `Copy as cURL(bash)`，程序要求输入cURL时，直接粘贴即可。
---
+++ main.py
@@ -0,0 +1,205 @@
+import requests
+import uncurl
+import time
+import json
+
+courses_dicts = [] #a temp list to store course_info dicts
+leaf_dicts = [] 
+
+def getProfile():
+    def getParams():
+        curl = input('Your curl: ')
+        curl_parsed = uncurl.parse_context(curl)
+        cookies = dict(curl_parsed.cookies)
+        headers = dict(curl_parsed.headers)
+        return cookies, headers
+    cookies, headers = getParams()
+    url = 'https://next.xuetangx.com/api/v1/u/user/basic_profile/'
+    profile = requests.get(url, cookies=cookies).json()
+    return profile, cookies, headers
+profile, cookies, headers = getProfile()
+
+class Student:
+    def __init__(self, profile):
+        self.name = profile['data']['name']
+        self.school = profile['data']['school']
+
+    def greet(self):
+        print(f'Logged in as {self.name}@{self.school}')
+
+    def getAllCoursesData(self):
+        # key "x-csrftoken' must be included or there'll be no returned value.
+        url = 'https://next.xuetangx.com/api/v1/lms/user/user-courses/'
+        courses = requests.get(url, cookies=cookies, headers=headers).json()
+        return courses
+
+    def listAllCourses(self):
+        courses_data = self.getAllCoursesData()
+        course_count = courses_data['data']['count']
+        courses = courses_data['data']['product_list']
+        print('No      SIGN                 CID          Course')
+        order = 0
+        for course in courses:
+            sign = course['sign']
+            cid = course['classroom_id']
+            name = course['name']
+            print(f'{order}      {sign}      {cid}      {name}')
+            courses_dicts.append({"order": order, "sign": sign, "cid": cid, "name": name})
+            order +=1
+
+class Course():
+
+    def __init__(self, sign, cid, cname):
+        self.sign = sign
+        self.cid = cid
+        self.cname = cname #course_name
+
+    def getCourseSchedule(self):
+        url = 'https://next.xuetangx.com/api/v1/lms/learn/get_evaluation_detail/'
+        params = (
+            ('sign', self.sign),
+            ('cid', self.cid),
+        )
+        evaluation_detail = requests.get(url, cookies=cookies, headers=headers, params=params).json()
+        return evaluation_detail
+
+    def listCourseInfo(self):
+        schedule = self.getCourseSchedule()['data']['score_detail'][0]['schedule']
+        cname = self.cname
+        print(f'Course: {cname}     Progress: {schedule}')
+
+    def getUncommitedProblems(self):
+
+        def getUncompletedLeaves():
+            chapters = self.getCourseSchedule()['data']['score_detail'][0]['resource']
+            for chapter in chapters:
+                if 'leaf_list' in chapter: # Chapter is directly divided into leaves
+                    leaves = chapter['leaf_list']
+                    for leaf in leaves:
+                        if leaf['schedule'] != 100: # one or several problem(s) not commited
+                            leaf_id = leaf['id']
+                            leaf_type_id = requests.get('https://next.xuetangx.com/api/v1/lms/learn/leaf_info/'+ str(self.cid) +'/'+ str(leaf_id)+'/', headers=headers,cookies=cookies, params={"sign":self.sign}).json()['data']['content_info']['leaf_type_id']
+                            leaf_dicts.append({'leaf_id':leaf_id,
+                                                'leaf_type_id': leaf_type_id
+                                                  }
+                                             )
+                        else:
+                            pass
+                else: # Chapter is divided into sections and then leaves
+                    sections = chapter['section_list']
+                    for section in sections:
+                        leaves = section['leaf_list']
+                        for leaf in leaves:
+                            leaf_id = leaf['id']
+                            leaf_type_id = requests.get('https://next.xuetangx.com/api/v1/lms/learn/leaf_info/'+ str(self.cid) +'/'+ str(leaf_id)+'/', headers=headers,cookies=cookies, params={"sign":self.sign}).json()['data']['content_info']['leaf_type_id']
+                            leaf_dicts.append({'leaf_id': leaf_id,
+                                               'leaf_type_id': leaf_type_id
+                                               }
+                                              )
+
+        def getUncompletedProblems():
+            for leafinfo in leaf_dicts:
+                leaf_type_id = leafinfo['leaf_type_id']
+                url = 'https://next.xuetangx.com/api/v1/lms/exercise/get_exercise_list/' + str(leaf_type_id) + '/'
+                data = problems = requests.get(url, cookies=cookies, headers=headers).json()['data']
+                problems = data['problems']
+                leafinfo['problem_ids'] = [] # a temp list to store problem ids
+                for problem in problems:
+                    if 'submit_time' in problem['user']:
+                        pass
+                    else:
+                        leafinfo['problem_ids'].append(problem['problem_id'])
+        getUncompletedLeaves()
+        getUncompletedProblems()
+        # 
+        # [
+        #     {'leaf_id': 11231651, 'leaf_type_id': 1651561561, 'problem_ids':[1,2,3,4,6,7]}
+        #     .....
+        #     .....
+        # ]
+    def getAnswerToProblem(self, leaf_type_id, problem_id):
+        # Leaf as a unit
+        exercise_url = 'https://next.xuetangx.com/api/v1/lms/exercise/get_exercise_list/' + str(leaf_type_id) + '/'
+        exercise_dict = requests.get(exercise_url, headers=headers,cookies=cookies).json()
+        problems = exercise_dict['data']['problems']
+        for problem in problems:
+            if problem_id == problem['problem_id']:
+                problem_type = problem['content']['Type']
+                if problem_type == 'SingleChoice':
+                    answer = problem['user']['answer'] # list
+                elif problem_type == 'FillBlank':
+                    answer = problem['user']['answers']  # a dict
+                elif problem_type == 'Judgement':
+                    answer = problem['user']['answer']  # list
+                elif problem_type == 'MultipleChoice':
+                    answer = problem['user']['answer']  # a list
+                elif problem_type == 'ShortAnswer': #No answer
+                    answer = None
+        return answer
+
+    def problemApply(self, leaf_id, leaf_type_id, problem_id, answer):
+       if answer == None:
+           pass
+       else:
+            headers_apply = headers
+            headers_apply['content-type'] = 'application/json'
+            if isinstance(answer,dict):
+                data = {
+                    "leaf_id": leaf_id,
+                    "classroom_id": self.cid,
+                    "exercise_id": leaf_type_id,
+                    "problem_id": problem_id,
+                    "sign": self.sign,
+                    "answers":answer, # FillBlank
+                    "answer": [] # SingleChoice, MultipleChoice, Judgement
+                }
+            else:
+                data = {
+                    "leaf_id": leaf_id,
+                    "classroom_id": self.cid,
+                    "exercise_id": leaf_type_id,
+                    "problem_id": problem_id,
+                    "sign": self.sign,
+                    "answers":{}, # FillBlank
+                    "answer": answer # SingleChoice, MultipleChoice, Judgement
+                }
+            response = requests.post('https://next.xuetangx.com/api/v1/lms/exercise/problem_apply/', headers=headers_apply, cookies=cookies,data=str(data).replace('\'','\"'))
+            print(response.text)
+
+# Create a Student instance
+student = Student(profile)
+
+# A simple greeting
+student.greet()
+
+# Print all the courses you are learning
+student.listAllCourses()
+
+# Select a course and instantiate a course  
+num = input('The one you choose(input the order num): ')
+for each in courses_dicts:
+    if int(num) == each['order']:
+        cname = each['name']
+        cid = each['cid']
+        sign = each['sign']
+        course = Course(sign, cid, cname)
+    else:
+        pass
+courses_dicts.clear()
+
+# Print name and progress of choosen course
+course.listCourseInfo()
+
+# Acquire all uncommited problems 
+course.getUncommitedProblems()
+
+# Answer all the problems by a  for-in iteration 
+for leafinfo in leaf_dicts:
+    leaf_id = leafinfo['leaf_id']
+    leaf_type_id = leafinfo['leaf_type_id']
+    problem_ids = leafinfo['problem_ids']
+    for problem_id in problem_ids:
+        answer = course.getAnswerToProblem(leaf_type_id, problem_id)
+        course.problemApply(leaf_id, leaf_type_id, problem_id, answer)
+        time.sleep(3)
+print("Done")
